47836317b478b33f57cc665ec1712b2e496ff895
Finha <finha@MacBook-Air-de-Finha.local>
2020-04-27 00:20:28 -0300

    Adição de rotas e criação da página inicial

---
+++ src/pages/main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class Main extends Component {
+  static navigationOptions = {
+    title: "JSHunt"
+  };
+
+  render() {
+    return (
+      <View>
+        <Text>Página Main</Text>
+      </View>
+    );
+  }
+}
