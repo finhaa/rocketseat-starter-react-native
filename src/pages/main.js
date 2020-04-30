822db969128e95b2e7e5c6fa8751762f3fb426f5
Rafael Farias <finha.farias@outlook.com>
2020-04-30 18:41:59 -0300

    Infinite Scroll e renderização de WebView

--- src/pages/main.js
+++ src/pages/main.js
@@ -8,20 +8,26 @@ export default class Main extends Component {
   };
 
   state = {
-    docs: []
+    productInfo: {},
+    docs: [],
+    page: 1
   };
 
   componentDidMount() {
     this.loadProducts();
   }
 
-  loadProducts = async () => {
-    const response = await api.get('/products');
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
 
     //Desestruturação
-    const { docs } = response.data;
+    const { docs, ...productInfo } = response.data;
 
-    this.setState({ docs });
+    this.setState({
+      docs: [...this.state.docs, ...docs],
+      productInfo,
+      page
+    });
   }
 
   //O renderItem do FlatList recebe vários parâmetros
@@ -31,11 +37,25 @@ export default class Main extends Component {
       <Text style={styles.productTitle}>{item.title}</Text>
       <Text style={styles.productDescription}>{item.description}</Text>
 
-      <TouchableOpacity style={styles.productButton} onPress={() => { }}>
+      <TouchableOpacity
+        style={styles.productButton}
+        onPress={() => {
+          this.props.navigation.navigate('Product', { product: item });
+        }}>
         <Text style={styles.productButtonText}>Acessar</Text>
       </TouchableOpacity>
     </View>
   )
+
+  loadMore = () => {
+    const { page, productInfo } = this.state;
+
+    if (page == productInfo.pages) return;
+
+    const pageNumber = page + 1;
+
+    this.loadProducts(pageNumber);
+  }
 
   render() {
     return (
@@ -45,6 +65,8 @@ export default class Main extends Component {
           data={this.state.docs}
           keyExtractor={item => item._id}
           renderItem={this.renderItem}
+          onEndReached={this.loadMore}
+          onEndReachedThreshold={0.1}
         />
       </View>
     );
