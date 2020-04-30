cc4f6dc810b330c14a5b5f49299fd18d2b1768ce
Rafael Farias <finha.farias@outlook.com>
2020-04-30 18:07:26 -0300

    Busca de dados da API e estilização da lista

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,16 +1,96 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import api from '../services/api';
 
 export default class Main extends Component {
   static navigationOptions = {
     title: "JSHunt"
   };
 
+  state = {
+    docs: []
+  };
+
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async () => {
+    const response = await api.get('/products');
+
+    //Desestruturação
+    const { docs } = response.data;
+
+    this.setState({ docs });
+  }
+
+  //O renderItem do FlatList recebe vários parâmetros
+  //Mas podemos usar desestruturação para pegar o item de iteração
+  renderItem = ({ item }) => (
+    <View style={styles.productContainer}>
+      <Text style={styles.productTitle}>{item.title}</Text>
+      <Text style={styles.productDescription}>{item.description}</Text>
+
+      <TouchableOpacity style={styles.productButton} onPress={() => { }}>
+        <Text style={styles.productButtonText}>Acessar</Text>
+      </TouchableOpacity>
+    </View>
+  )
+
   render() {
     return (
-      <View>
-        <Text>Página Main</Text>
+      <View style={styles.container}>
+        <FlatList
+          contentContainerStyle={styles.list}
+          data={this.state.docs}
+          keyExtractor={item => item._id}
+          renderItem={this.renderItem}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa'
+  },
+  list: {
+    padding: 20
+  },
+  productContainer: {
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20
+  },
+  productTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: '#333'
+  },
+  productDescription: {
+    fontSize: 16,
+    color: '#999',
+    marginTop: 5,
+    lineHeight: 24
+  },
+  productButton: {
+    height: 42,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#DA552F',
+    backgroundColor: 'transparent',
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10
+  },
+  productButtonText: {
+    fontSize: 16,
+    color: '#DA552F',
+    fontWeight: "bold"
+  }
+})
